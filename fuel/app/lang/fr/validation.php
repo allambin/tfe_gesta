a5a6b5571631dcdf05346d6db584ba1285857ae0
Cranberry <annelise.lambin@gmail.com>
2014-02-14 20:53:06 +0100

    traduit les erreurs en français

---
+++ fuel/app/lang/fr/validation.php
@@ -0,0 +1,20 @@
+<?php
+
+return array(
+	'required'        => 'Le champ :label est requis et doit contenir une valeur.',
+	'min_length'      => 'Le champ :label doit contenir au minimum :param:1 caractères',
+	'max_length'      => 'Le champ :label ne peut pas contenir plus de :param:1 caractères.',
+	'exact_length'    => 'Le champ :label doit contenir exactement :param:1 caractères.',
+	'match_value'     => 'Le champ :label doit contenir la valeur :param:1.',
+	'match_pattern'   => 'Le champ :label doit correspondre au motif :param:1.',
+	'match_field'     => 'Le champ :label doit correspondre au champ :param:1.',
+	'valid_email'     => 'Le champ :label doit contenir une adresse email valide.',
+	'valid_emails'    => 'Le champ :label doit contenir une liste d\'adresses email valides.',
+	'valid_url'       => 'Le champ :label doit contenir une URL valide.',
+	'valid_ip'        => 'Le champ :label doit contenir une adresse IP valide.',
+	'numeric_min'     => 'La valeur numérique minimale de :label doit être :param:1',
+	'numeric_max'     => 'La valeur numérique maximale de :label doit être :param:1',
+	'numeric_between' => 'Le champ :label doit contenir une valeur numérique entre :param:1 et :param:2',
+	'valid_string'    => 'La règle :rule(:param:1) a échoué pour le champ :label',
+	'required_with'   => 'Le champ :label doit contenir une valeur si :param:1 contient une valeur.',
+);
